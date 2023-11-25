eacac5f370f13c8ac48629d01c20872e6000cad1
ilyha787 <104492271+ilyha787@users.noreply.github.com>
2023-11-25 12:09:26 +0300

    я лох

--- Svyaznoi/Svyaznoi.API/Controllers/NakladnayaController.cs
+++ Svyaznoi/Svyaznoi.API/Controllers/NakladnayaController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Svyaznoi.Context;
 using Svyaznoi.Context.Contracts.Models;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
 
 namespace Svyaznoi.API.Controllers
 {
@@ -8,12 +11,46 @@ namespace Svyaznoi.API.Controllers
     [Route("[controller]")]
     public class NakladnayaController : ControllerBase
     {
-        [HttpGet] //localhost:111224/group
-        public IActionResult GetAllNakladnaya() 
+        private readonly IContext context1;
+        public NakladnayaController(IContext context1)
         {
-            var context = new SVContext();
-            var nakladnayalist = context.Nakladnayas.ToList();
+            this.context1 = context1;
+        }
+        [HttpGet] //localhost:111224/group
+        public IActionResult GetAllNakladnaya()
+        {
+            var nakladnayalist = context1.Nakladnayas.ToList();
             return Ok(nakladnayalist);
+        }
+        [HttpGet("{id:Guild}")]
+        public IActionResult Get(Guid id)
+        {
+            var nakladnayalist = context1.Nakladnayas.FirstOrDefault(x => x.Id == id);
+            return Ok(nakladnayalist);
+        }
+        [HttpPost]
+        public IActionResult Create(GroupRequestApiModel model)
+        {
+            var item = new Nakladnaya
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                Description = model.Description,
+                CreatedAT = DateTime.Now,
+                CreatedBy = "Я",
+                UpdatedAt = DateTime.Now,
+                UpdatedBy = "Я"
+            };
+        }
+        [HttpDelete("{id}")] 
+        public IActionResult Delete(Guid id) 
+        {
+            var group = context1.Nakladnayas.FirstOrDefault(x => x.Id == id);
+            if (group != null)
+            {
+                context1.Nakladnayas.Remove(group);
+            }
+            return Ok();
         }
     }
 }
